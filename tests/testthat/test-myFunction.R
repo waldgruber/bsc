e1de1377596adfb2ed5495a325579fd1afd0ca44
Sonja Waldgruber <sonja@waldgruber.at>
2015-11-24 21:20:36 +0100

    L added

--- tests/testthat/test-myFunction.R
+++ tests/testthat/test-myFunction.R
@@ -1,6 +1,6 @@
 test_that('my first test', {
   expect_equal(
     myFunction(1:10, rep(1:2, each = 5)),
-    data.frame(group = c(1, 2), avg = c(3, 8))
+    data.frame(group = c(1L, 2L), avg = c(3, 8))
   )
 })
