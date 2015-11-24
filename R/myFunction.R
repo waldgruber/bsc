049200cf6a5bb7356cb756d4d6b38d9bf30e3dcb
Sonja Waldgruber <sonja@waldgruber.at>
2015-11-24 09:04:48 +0100

    myFunction

---
+++ R/myFunction.R
@@ -0,0 +1,16 @@
+#' Computes means by groups
+#' @description
+#' You can put extended description here
+#' @details
+#' If your function is complicated, you may consider adding a details section
+#' @param values numeric vector of values
+#' @param groups vector of groups
+#' @return data.frame with groups names in first column and average values per group in the second one
+#' @export
+#' @import dplyr
+myFunction = function(values, groups){
+  result = data.frame(value = values, group = groups) %>%
+    group_by_('group') %>%
+    summarize_(avg = ~mean(value, na.rm = TRUE))
+  return(result)
+}
